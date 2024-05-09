a7ee779c37269e5e629d56700e92adb413769d91
jacintoaisa <jacintoaisa@gmail.com>
2024-05-09 12:14:04 +0200

    Add files via upload

---
+++ Furgoneta.cs
@@ -0,0 +1,26 @@
+﻿using ConsoleAppClases.Modelo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppClases.Garage
+{
+    public class Furgoneta : IAlquilable
+    {
+        public string Identificador { get; set; }
+        public double PrecioHora { get; set; } = 5;
+        public double Tiempo { get; set; }
+
+        public double dameCoste()
+        {
+            return PrecioHora * Tiempo;
+        }
+
+        public override string? ToString()
+        {
+            return $"Hola soy una Furgoneta con precioHora {this.PrecioHora}";
+        }
+    }
+}
